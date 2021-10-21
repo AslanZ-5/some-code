0339a12f382a70bfb6b85e0d638713fdb3741e7b
Aslan <asl.zurabov@gmail.com>
2021-10-21 10:43:00 +0300

    private variables

--- OOP/test2.py
+++ OOP/test2.py
@@ -1,10 +1,10 @@
 class Nameofclass():
     class_attribute = 'value'
     def __init__(self,param1,param2):
-        self.param1 = param1
-        self.param2 = param2
+        self._param1 = param1
+        self._param2 = param2
     def method(self):
-        return self.param2 + self.param1
+        return self._param2 + self._param1
     @classmethod
     def cls_methd(cls,param1,param2):
         return cls(param1,param2)
