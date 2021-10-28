ec6ea42a8d5c8a1ddb92845fd610fc0ea8f4cb74
Aslan <asl.zurabov@gmail.com>
2021-10-28 09:20:21 +0300

    oop

---
+++ OOP/test4.py
@@ -0,0 +1,21 @@
+class Person:
+    def __init__(self, name, job=None, pay=0):
+        self.name = name
+        self.job = job
+        self.pay = pay
+
+    def lastName(self):
+        return self.name.split()[-1]
+
+    def giveRaise(self, percent):
+        self.pay = int(self.pay * (1 + percent))
+
+    def __repr__(self):
+        return '[Person: %s, %s]' % (self.name, self.pay)
+
+
+class Manager(Person):
+    def giveRaise(self, percent, bonus=.10):  # Redefine at this level
+        Person.giveRaise(self, percent + bonus)
+
+if __name__ == '__main__':
