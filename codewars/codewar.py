b11b5e3514b9f4f2bbd09f3ddff3b929c4f6ce37
Aslan <asl.zurabov@gmail.com>
2021-11-20 23:07:09 +0300

    ip validator

--- codewars/codewar.py
+++ codewars/codewar.py
@@ -95,9 +95,23 @@ def rotate(matrix, direction):
     if direction == "clockwise":
         return [list(reversed(i)) for i in zip(*r)]
 
+
 def numbers(*args):
     a = list(args)
-    c= [ True if  isinstance(i,(int,float)) else False  for i in a  ]
+    c = [True if isinstance(i, (int, float)) else False for i in a]
     return all(c)
+
+
 print(numbers())
 
+# ip  adress validator
+number = int(input())
+
+for n in range(number):
+    string = input()
+    if re.search(r'^([a-f0-9]{0,4}:[a-f0-9]{0,4}:[a-f0-9]{0,4}:[a-f0-9]{0,4}:[a-f0-9]{0,4}:[a-f0-9]{0,4}:[a-f0-9]{0,4}:[a-f0-9]{0,4})$', string):
+        print('IPv6')
+    elif re.search(r'^(([0-9]|[0-9]{2}|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[0-9]{2}|1[0-9]{2}|2[0-4][0-9]|25[0-5])$', string):
+        print('IPv4')
+    else:
+        print('Neither')
